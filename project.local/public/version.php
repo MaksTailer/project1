831e5dd15a12762236822298f7734e37677c345c
Max Tailer <maximb904@gmail.com>
2024-05-30 09:50:19 +0300

    Update

--- project.local/public/version.php
+++ project.local/public/version.php
@@ -11,14 +11,13 @@ try {
     // Создаем экземпляр PDO для соединения с базой данных
     $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-    // Выполняем SQL-запрос для получения версии PostgreSQL
-    $stmt = $pdo->query('SELECT version()');
-    $version = $stmt->fetchColumn();
+    $conn = $pdo->query('SELECT version()');
+    $version = $conn->fetchColumn();
 
-    // Выводим версию PostgreSQL
-    echo "PostgreSQL version: " . $version;
-} catch (PDOException $e) {
-    // Обработка ошибок подключения к базе данных
-    echo "Error: " . $e->getMessage();
-}
+    echo "PostgreSQL version: " . $version; 
+    
+    } catch (PDOException $e) {
+        // Обработка ошибок подключения к базе данных
+        echo "Error: " . $e->getMessage();
+    }
 ?>
